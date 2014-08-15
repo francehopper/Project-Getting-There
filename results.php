444e1a6654f74a5fec55b05e9a88f04b1a6c5ee8
Stephen J. Weber <Stephen@Totoro.hppr.co>
2014-08-15 11:54:24 -0400

    Hello, Planet.
    First test version.

---
+++ results.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html>
+<head>
+	<title>Next Scheduled Arrivals</title>
+</head>
+<body>
+<?php
+// http://www3.septa.org/hackathon/BusSchedules/?req1=17842&req2=17&req3=i&req6=7&callback=?
+// Parms(s): ?req1=stop_id req2=route (optional) req3=(i or o) (for inbound/outbound)optional req=6 number of results(optional)
+// fetch stop ID, route, and results limit
+$stopID = $_GET['stopID'];
+$routeNo = $_GET['routeNo'];
+$arrivalsLimit = $_GET['arrivalsLimit'];
+// connect to SEPTA API
+$json = file_get_contents('http://www3.septa.org/hackathon/BusSchedules/?req1=$stopID&req2=$routeNo&req6=$arrivalsLimit&callback=?');
+// get data from JSON feed
+$data = json_decode($json, true);
+// parse results
+foreach ($data['$routeNo'] as $line) { 
+    $name = $line['StopName']; // get name of the stop
+    $route =  $line['Route']; // get route number
+    $when = $line['DateCalender']; // get when it will arrive
+    $message = '<li>Route '.$route.' is expected to arrive at '.$name.' at '.$when.'.</li>';
+    echo $message; // return the status
+}
+?>
+</body>
+</html>
