a9956a2901156da3b3f444cea05bd11b08f9a56e
Stephen J. Weber <Stephen@Totoro.hppr.co>
2014-08-15 12:13:47 -0400

    Getting there.
    Initial public beta release.

--- results.php
+++ results.php
@@ -11,18 +11,21 @@
 $stopID = $_GET['stopID'];
 $routeNo = $_GET['routeNo'];
 $arrivalsLimit = $_GET['arrivalsLimit'];
+// build JSON URL
+$fetchThis = 'http://www3.septa.org/hackathon/BusSchedules/?req1='.$stopID.'&req2='.$routeNo.'&req6='.$arrivalsLimit;
 // connect to SEPTA API
-$json = file_get_contents('http://www3.septa.org/hackathon/BusSchedules/?req1=$stopID&req2=$routeNo&req6=$arrivalsLimit&callback=?');
+$json = file_get_contents($fetchThis);
 // get data from JSON feed
 $data = json_decode($json, true);
 // parse results
-foreach ($data['$routeNo'] as $line) { 
+foreach ($data['17'] as $line) { 
     $name = $line['StopName']; // get name of the stop
     $route =  $line['Route']; // get route number
     $when = $line['DateCalender']; // get when it will arrive
     $message = '<li>Route '.$route.' is expected to arrive at '.$name.' at '.$when.'.</li>';
     echo $message; // return the status
 }
+echo '<a href="index.html">Return home</a>';
 ?>
 </body>
 </html>
